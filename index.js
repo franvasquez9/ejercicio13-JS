44fcc0fcbcd57d746b6c6f69392c25ed62ddfb13
franvasquez9 <89528074+franvasquez9@users.noreply.github.com>
2022-07-05 12:43:55 -0500

    ejercicio 13 JS

---
+++ index.js
@@ -0,0 +1,5 @@
+let text_sng = "Hola soy un texto con comillas simples"
+let text_dbl = "Hola soy un texto con comillas dobles"
+let text_backticks = "Hola esto es un string con backticks"
+
+console.log(text_sng, text_dbl, text_backticks)
